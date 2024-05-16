854d1155de42184441fda7dc0b573fa743f1b184
CristianN01 <cristiannechifor71@gmail.com>
2024-05-16 19:01:12 +0200

    risolto esercizio pari o dispari aggiungendo la scelta del giocatore di essere pari o dispari

--- Pari e Dispari/JS/script.js
+++ Pari e Dispari/JS/script.js
@@ -1,8 +1,6 @@
  const evenOrOdd =prompt('scegli Pari o Dispari');
 const clientNumber =Number.parseInt(prompt('Scegli un numero da 1 a 5'), 10);
 
-const trueP = pari;
-const falseD = dispari;
 
 function computerNumberRandom(min, max){
     return random = Math.floor(Math.random()*(max-min+1)+min);
@@ -16,15 +14,20 @@ function numbersSum(){
 }
 let risultato = numbersSum();
 
- if (risultato % 2 == 0 || evenOrOdd === trueP){
-     console.log('Il tuo numero' + '' + clientNumber)
-     console.log('Numero del computer' + '' + random)
-     console.log('La somma e stata:' + '' + risultato)
+const even = 'pari';
+
+
+ if (risultato % 2 == 0 && even === evenOrOdd){
+     console.log('Il tuo numero:' + ' ' + clientNumber)
+     console.log('Numero del computer:' + ' ' + random)
+     console.log('La somma e stata:' + ' ' + risultato)
      console.log('Hai vinto!!!')
- } else
-    if (risultato % 2 == 1 || evenOrOdd === falseD)
-    console.log('Il tuo numero' + '' + clientNumber)
-    console.log('Numero del computer' + '' + random)
-    console.log('La somma e stata:' + '' + risultato)
-     console.log('Hai perso... riprova la prossima volta :D')
+ } else{
+     if (risultato % 2 == 1 && even != evenOrOdd)
+     console.log('Il tuo numero:' + ' ' + clientNumber)
+     console.log('Numero del computer:' + ' ' + random)
+     console.log('La somma e stata:' + ' ' + risultato)
+      console.log('Hai perso... riprova la prossima volta :D')
+
+ }
  
