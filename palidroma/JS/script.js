7840abf0c9edbff53c3737ad5e0df786699e2fd5
CristianN01 <cristiannechifor71@gmail.com>
2024-05-16 16:59:48 +0200

    creato funzione per invertire la mia stringa

---
+++ palidroma/JS/script.js
@@ -0,0 +1,19 @@
+
+// const myPaliWord = prompt('type a word');
+
+function reversedString(string){
+    let newString = '';
+
+    for (let i = string.length - 1; i >= 0; i--){
+        newString += string[i];
+    }
+    return newString;   
+} 
+reversedString('hello');
+console.log(string)
+
+
+function isItPalindromWord(word){
+    
+}
+
